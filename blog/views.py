260accc69b75eba203a311c67112a700912e579a
frapplecat <nigel1138@hotmail.com>
2023-04-24 13:07:22 +0000

    Create first View for blogposts

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from django.views import generic
 from .models import Post
 
 
-class PostList(generic.ListVew):
+class PostList(generic.ListView):
     model = Post
-    queryset = Post.object.filter(status=1).order_by('-created_on')
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
