ecc8b365515858983e28ee97fe334817698678c8
frapplecat <nigel1138@hotmail.com>
2023-04-25 07:24:37 +0000

    Add approve/delete functionality for comments in Admin Panel

--- blog/admin.py
+++ blog/admin.py
@@ -10,7 +10,7 @@ class PostAdmin(SummernoteModelAdmin):
     list_filter = ('status', 'created_on')
     list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ('title', 'content')
-    summernote_fields = ('content')
+    summernote_fields = ('content',)
 
 
 @admin.register(Comment)
@@ -19,7 +19,7 @@ class CommentAdmin(admin.ModelAdmin):
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
     search_fields = ('name', 'email', 'body')
-    actions = ['approved_comments']
+    actions = ['approve_comments']
 
     def approve_comments(self, request, queryset):
         queryset.update(approved=True)
